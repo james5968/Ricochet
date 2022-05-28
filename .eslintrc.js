3684f145e0f11e244a4484ad7058c38f8c2834f9
James Ashford <git@james-ashford.com>
2022-05-28 13:34:39 +0100

    Project init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'prettier/prettier': ['error'],
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
